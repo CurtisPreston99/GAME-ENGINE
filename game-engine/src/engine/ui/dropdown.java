29f45d0e52f65313c364971258113470a3d6b738
wizordgamming <wizordgamming@gmail.com>
2020-07-13 15:03:51 +1200

    finished implementing dropdown
    Signed-off-by: wizordgamming <wizordgamming@gmail.com>

--- game-engine/src/engine/ui/dropdown.java
+++ game-engine/src/engine/ui/dropdown.java
@@ -10,7 +10,9 @@ public class dropdown extends UIelement {
     String[] options;
     int selected=0;
     boolean down=false;
+    Boolean canDrop = true;
     int over=0;
+
     public dropdown(int x, int y, int sizex,int sizey,String[] options, scene s, window w, String name) {
         super(x, y, sizex, sizey, s, w, name);
         this.options=options;
@@ -18,11 +20,22 @@ public class dropdown extends UIelement {
 
     @Override
     public void update(window w) {
-        if(down){
+        mouse mouse =w.input.Mouse;
 
+        if(down){
+            
+            int Mx=mouse.X();
+            int My=mouse.Y();
+            if(down&& Mx>x&&Mx<x+sizex){
+            int Ny=(My-(y+sizey))/sizey;
+            if(Ny<options.length){
+                // System.out.println(Ny);
+                // System.out.println(options[Ny]);
+                over=Ny;
+                }
+            }
         }
     }
-
     @Override
     public void draw(PGraphics b) {
         if(hover()){
@@ -58,12 +71,22 @@ public class dropdown extends UIelement {
         if(mouse.left){
             int Mx=mouse.X();
             int My=mouse.Y();
-            if(Mx>x&&Mx<x+sizex && My>y&&My<y+sizey ){
+            if(Mx>x&&Mx<x+sizex && My>y&&My<y+sizey &&canDrop){
                 down=!down;
-            }else
-            if(down){
-                
+                canDrop=false;
+            }else{
+            if(down&& Mx>x&&Mx<x+sizex){
+                int Ny=(My-(y+sizey))/sizey;
+                if(Ny<options.length){
+                    // System.out.println(Ny);
+                    // System.out.println(options[Ny]);
+                    selected=Ny;
+                }
+
             }
+        }
+        }else{
+            canDrop=true;
         }
 
     }
